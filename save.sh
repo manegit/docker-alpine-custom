ffda744722e718eda049ca2d60f8a0fe8eb806a1
manegit <81263792+manegit@users.noreply.github.com>
2023-03-30 21:34:13 +0200

    Update save.sh

--- save.sh
+++ save.sh
@@ -1,4 +1,4 @@
-docker image save -o /tmp/docker-alpine-custom.tar mane1docker/alpine-custom:latest
+docker image save -o /tmp/docker-alpine-custom.tar ghcr.io/manegit/docker-alpine-custom:latest
 gzip /tmp/docker-alpine-custom.tar
 mv -f /tmp/docker-alpine-custom.tar.gz ~/
 
